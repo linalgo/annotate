62f57f7356e7aa47bc8bcb10a7afcd011d732101
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-05-03 12:43:39 +0200

    First attempt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-let f = document.getElementById('first').firstChild;
-console.log(f);
+import * as data from './db.json';
 
+let f = document.getElementById('first').firstChild;
 let s = document.createElement('span');
 s.style.backgroundColor = 'yellow';
 
@@ -13,18 +13,53 @@ let range: any;
 document.addEventListener("selectionchange", function() {
   let selection = window.getSelection();
   range = selection.getRangeAt(0);
-
-  console.log(range);
+  // console.log(range);
 });
 
 function highlight(range: Range): boolean {
+  console.log(range);
   const s = document.createElement('span');
   s.style.backgroundColor = 'yellow';
   range.surroundContents(s);
   return true;
 }
 
-let btn = document.createElement("button");
-btn.innerHTML = 'highlight';
+let btn = document.getElementById("button");
 btn.addEventListener("click", (e:Event) => highlight(range));
-document.body.appendChild(btn);
+
+console.log(data);
+console.log((data as any)[5].target[0])
+
+
+function annotate(annotation: any) {
+  let nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT)
+
+  let start: number = <any>annotation.target[0].selector[1].start
+  let end: number = <any>annotation.target[0].selector[1].end
+
+  let position = 0;
+  let node: Node;
+  let l: number;
+  let r2 = document.createRange();
+  while (node = nodeIterator.nextNode()) {
+    l = node.nodeValue.length
+    if (position + l >= start && start - position >= 0) {
+      console.log("startOffset", start - position)
+      r2.setStart(node, start - position)
+    }
+    if (position + l >= end && start - position >= 0) {
+      r2.setEnd(node, end - position)
+      console.log("stopOffset", end - position)
+    }
+    position += l;
+  }
+  console.log(r2);
+  let s2 = document.createElement('span');
+  s2.style.backgroundColor = 'blue';
+  r2.surroundContents(s2)
+}
+
+for (let i = 0; i < 6; i++) {
+ let annotation = (<any>data)[i];
+ annotate(annotation)
+}
