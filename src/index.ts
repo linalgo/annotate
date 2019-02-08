f36cf0360302706d1ce4406dd2c4410a468d3d73
Arnaud Rachez <arnaud.rachez@gmail.com>
2019-02-08 16:01:45 +0100

    issue #4 - use annotation interface

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 export * from './annotators';
 export * from './selectors';
 
-import { Annotator } from './annotators/annotator';
+// import { Annotator } from './annotators/annotator';
 
-let range: Range;
-document.addEventListener('selectionchange', function() {
-  let selection = window.getSelection();
-  range = selection.getRangeAt(0);
-});
+// let range: Range;
+// document.addEventListener('selectionchange', function() {
+//   let selection = window.getSelection();
+//   range = selection.getRangeAt(0);
+// });
 
-function highlight(range: Range) {
-  const annotator = new Annotator();
-  annotator.annotate(range);
-}
+// function highlight(range: Range) {
+//   const annotator = new Annotator();
+//   annotator.annotate(range);
+// }
 
-let btn = document.getElementById('button');
-btn.addEventListener('click', (e: Event) => highlight(range));
+// let btn = document.getElementById('button');
+// btn.addEventListener('click', (e: Event) => highlight(range));
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
